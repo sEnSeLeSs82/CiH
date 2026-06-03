06b1fdbd00376e1ec7ef1cf604f85ebbc6e1ac4c
sEnSeLeSs82 <s.mewes@heller-kopf.de>
2026-06-03 22:25:37 +0200

    noch einige anpassungen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 
 import os, sys, sqlite3
+import csv
 
 if os.path.exists("CashAufTäsch.db"):
     print("Datenbank vorhanden")
@@ -47,19 +48,38 @@ while stay=="n":
         
         connection.commit()
         saldo = (saldo - soll)
-       
-        print ("Es sind jetzt ", saldo, "€ im Saldo")
+        cursor.execute("SELECT SUM(betrag) FROM umsatz")
+        result = cursor.fetchone()
+        summe = round(result[0],2)
+        
+        print ("Es sind jetzt ", summe, "€ im Saldo")
         print("beenden? y oder n eingeben")
         stay=(input(""))       
         
     elif auswahl=="e":
         
         print("Du hast Einnahme gewählt")
+        print("Datum oder Grund der Einnahme?")
+        
+        grund_einnahme = input("")
+        name=grund_einnahme
         
         haben = einnahme()
+        
         saldo = (saldo + haben)
         
-        print ("Es sind jetzt ", saldo, "€ im Saldo")
+        betrag=haben
+        cursor.execute("""
+            INSERT INTO umsatz (name , betrag) VALUES (?, ?)
+            """, (name, betrag))
+        
+        connection.commit()
+        
+        cursor.execute("SELECT SUM(betrag) FROM umsatz")
+        result = cursor.fetchone()
+        summe = round(result[0],2)
+        
+        print ("Es sind jetzt ", summe, "€ im Saldo")
         
         print ("beenden? y oder n eingeben")
         stay=(input(""))
@@ -69,4 +89,30 @@ while stay=="n":
         print ("beenden? y oder n eingeben")
         stay=(input(""))
 
-connection.close()
+
+print("möchtensie die umsätze jetzt ausgeben - j oder n")
+frage_druck = input("")
+
+if frage_druck=="j":
+
+    # Verbindung zur Datenbank herstellen
+    connection = sqlite3.connect("CashAufTäsch.db")
+    cursor = connection.cursor()
+
+    # Daten aus der Tabelle auswählen
+    cursor.execute("SELECT * FROM umsatz")
+    rows = cursor.fetchall()
+
+    # In CSV-Datei schreiben
+    with open("ausgabe.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        # Optional: Header schreiben
+        # writer.writerow([desc[0] for desc in cursor.description])
+        writer.writerows(rows)
+        
+        connection.close()
+        
+else:
+    print("ok dann nicht")
+
+
